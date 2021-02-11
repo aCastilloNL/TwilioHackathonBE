015ed092f06b426f91dfc87c6e90b68f9e90dbf8
Alfred <castellum.a@gmail.com>
2021-02-11 12:38:46 +0100

    Updated babysitter title in routes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/babysitters', (req, res) => {
     console.log(req.body);
     member
     .save()
-    .then((member) => res.status('201').json(member))
+    .then((member) => res.status('201').json(babysitters))
     .catch((error) => res.sendStatus('500'));
 })
 
