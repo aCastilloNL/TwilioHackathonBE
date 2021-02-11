cb76a6fff0dc2d16457edf1d39d061176f6f3838
Viktoryia <kulinkovichva@gmail.com>
2021-02-11 15:13:58 +0300

    added test twilio route

--- app.js
+++ app.js
@@ -3,30 +3,33 @@ const app = express();
 
 const Babysitter = require('./models/Babysitter');
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 require('./database/config');
 
 app.get('/', (req, res) => {
-    res.send('Hey Twillio');
-})
+  res.send('Hey Twillio');
+});
+
+app.post('/test', (req, res) => {
+  console.log(req.body);
+  res.sendStatus('200');
+});
 
 app.get('/babysitters', (req, res) => {
-    Babysitter.find()
-    .then((members) => res.status('201').json(babysitters))
+  Babysitter.find()
+    .then((babysitters) => res.status('201').json(babysitters))
     .catch((error) => res.sendStatus('500'));
-})
-
-
+});
 
 app.post('/babysitters', (req, res) => {
-    const member = new Babysitter(req.body);
-    console.log(req.body);
-    member
+  const member = new Babysitter(req.body);
+  console.log(req.body);
+  member
     .save()
     .then((member) => res.status('201').json(member))
     .catch((error) => res.sendStatus('500'));
-})
+});
 
 module.exports = app;
--- database/config.js
+++ database/config.js
@@ -6,10 +6,3 @@ mongoose
   .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('connected to db'))
   .catch(() => console.log('error connecting to db'));
-
-
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
-    console.log('we are connected!')
-  });
